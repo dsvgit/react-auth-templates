72255ebc99041e1a30f6e9d20c86cde0b94e5482
Sergey Dedkov <dsv.mail@yandex.ru>
2023-06-18 02:36:45 +0600

    gh-pages deploy fixes

--- README.md
+++ README.md
@@ -10,3 +10,25 @@ Boilerplate for auth cases.
 pnpm install
 pnpm dev
 ```
+
+## Publishing troubleshooting
+
+### Cache
+remove `node_modules/.cache/gh-pages`
+
+### Access token
+"/dev/tty: No such a device or address"
+  If, when deploying, you get /dev/tty: No such a device or address or a similar error, try the following:
+
+1. Create a new Personal Access Token
+2. `git remote set-url origin https://<user>:<token>@github.com/<user>/<repo> .`
+3. Try `npm run deploy` again
+
+### Cannot read property 'email' of null
+
+"Cannot read property 'email' of null"
+If, when deploying, you get Cannot read property 'email' of null, try the following:
+
+1. `git config --global user.name '<your_name>'`
+2. `git config --global user.email '<your_email>'`
+3. Try `npm run deploy` again
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
-import * as path from 'path'
-import { defineConfig } from 'vite'
-import svgr from 'vite-plugin-svgr'
-import react from '@vitejs/plugin-react'
+import * as path from "path";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+import react from "@vitejs/plugin-react";
+import packageJson from "./package.json";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
-    }),
-    svgr(),
-  ],
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
-})
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "development" ? undefined : packageJson.homepage,
+    plugins: [
+      react({
+        babel: {
+          plugins: ["@emotion/babel-plugin"],
+        },
+      }),
+      svgr(),
+    ],
+    resolve: {
+      alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    },
+  };
+});
